ec137efb1339c73f0500ce9e48bd9271a2abd309
schadani <schadani@bluewin.ch>
2017-01-20 20:15:05 +0100

    cleaned up keymap.c

--- keyboards/ergodox/keymaps/schadani/keymap.c
+++ keyboards/ergodox/keymaps/schadani/keymap.c
@@ -9,20 +9,18 @@
 #define MDIA   2 // media layer
 #define SPEC   3 // special layer
 
-#define NmPad 10 // left symbol-shift key
-#define LMdia 11 // left media-shift key
-#define LSpec 12 // left special-shift key
-#define Symbl 13 // right symbol-shift key
-#define RMdia 14 // right media-shift key
-#define RSpec 15 // right special-shift key
+#define NUMPAD 10 // left symbol-shift key
+#define SPECTG 11 // left special-shift key
+#define SYMBOL 12 // right symbol-shift key
+#define MEDIA  13 // right media-shift key
 
 #define MUL   20 // mouse up left
 #define MUR   21 // mouse up right
 #define MDL   22 // mouse down left
 #define MDR   23 // mouse down right
 
-#define CO_PA M(30) // hold copy, tap paste
-#define CU_PA M(31) // hold cut, tap paste
+#define CO_PA 30 // hold copy, tap paste
+#define CU_PA 31 // hold cut, tap paste
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -51,16 +49,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // left hand
  KC_ESC  ,KC_1  ,KC_2   ,KC_3  ,KC_4  ,KC_5   ,KC_6
 ,KC_TAB  ,KC_Q  ,KC_W   ,KC_E  ,KC_R  ,KC_T   ,CU_PA
-,M(NmPad),KC_A  ,KC_S   ,KC_D  ,KC_F  ,KC_G
+,M(NUMPAD),KC_A  ,KC_S   ,KC_D  ,KC_F  ,KC_G
 ,KC_LSFT ,KC_Z  ,KC_X   ,KC_C  ,KC_V  ,KC_B   ,CO_PA
 ,KC_LCTL ,KC_DEL,KC_HOME,KC_END,KC_LALT
                                  ,LCTL(KC_MINS) ,KC_LCBR
                                                 ,KC_LPRN
-                               ,KC_SPC ,F(LSpec),KC_LBRC
+                               ,KC_SPC ,F(SPECTG),KC_LBRC
                                                                   // right hand
                                                                  ,KC_7    ,KC_8   ,KC_9   ,KC_0   ,KC_MINS ,KC_EQL   ,KC_GRV
-                                                                 ,KC_PGUP ,KC_Y   ,KC_U   ,KC_I   ,KC_O    ,KC_P     ,F(RMdia)
-                                                                          ,KC_H   ,KC_J   ,KC_K   ,KC_L    ,KC_SCLN  ,F(Symbl)
+                                                                 ,KC_PGUP ,KC_Y   ,KC_U   ,KC_I   ,KC_O    ,KC_P     ,F(MEDIA)
+                                                                          ,KC_H   ,KC_J   ,KC_K   ,KC_L    ,KC_SCLN  ,F(SYMBOL)
                                                                  ,KC_PGDN ,KC_N   ,KC_M   ,KC_COMM,KC_DOT  ,KC_SLSH  ,KC_RSFT
                                                                                   ,KC_RALT,KC_MEH ,KC_HYPR ,KC_DEL   ,KC_RCTL
                                                                  ,KC_RCBR ,LCTL(KC_EQL)
@@ -98,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS
                                              ,KC_TRNS ,KC_TRNS
                                                       ,KC_TRNS
-                                    ,KC_TRNS ,F(LSpec),KC_TRNS
+                                    ,KC_TRNS ,F(SPECTG),KC_TRNS
                                                                  // right hand
                                                                  ,KC_F7   ,KC_F8   ,KC_F9    ,KC_F10    ,KC_F11   ,KC_F12  ,KC_TRNS
                                                                  ,KC_TRNS ,KC_PIPE ,KC_KP_7  ,KC_KP_8   ,KC_KP_9  ,KC_SLSH ,KC_TRNS
@@ -155,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /******* Special Layer *************************************************************************************************
  *
  * ,-------------------------------------------------------.    ,-------------------------------------------------------.
- * |             |MdiaLk|SymbLk|CapsLk|      |      |      |    |      |      |      |      |      |      |             |
+ * |             |MdiaLk|SYMBOLk|CapsLk|      |      |      |    |      |      |      |      |      |      |             |
  * |-------------+------+------+------+------+-------------|    |------+------+------+------+------+------+-------------|
  * |             |      |      |  UP  |      |CTRL+T|      |    |      |      |      |      |      |      |             |
  * |-------------+------+------+------+------+------|      |    |      |------+------+------+------+------+-------------|
@@ -175,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [SPEC] = KEYMAP(
 // left hand
- KC_TRNS ,F(LMdia)  ,F(NmPad),KC_CAPS    ,KC_TRNS     ,KC_TRNS    ,KC_TRNS
+ KC_TRNS ,F(MEDIA)  ,F(NUMPAD),KC_CAPS    ,KC_TRNS     ,KC_TRNS    ,KC_TRNS
 ,KC_TRNS ,KC_TRNS   ,KC_TRNS ,KC_UP      ,KC_TRNS     ,LCTL(KC_T) ,KC_TRNS
 ,KC_TRNS ,LCTL(KC_A),KC_LEFT ,KC_DOWN    ,KC_RIGHT    ,LCTL(KC_F)
 ,KC_CAPS ,KC_TRNS   ,KC_TRNS ,LCTL(KC_S) ,LCTL(KC_Z)  ,KC_TRNS    ,KC_TRNS
@@ -196,13 +194,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-     // the faux shift keys are implemented as macro taps
-     [NmPad] = ACTION_MACRO_TAP(NmPad)
-    ,[LMdia] = ACTION_MACRO_TAP(LMdia)
-    ,[LSpec] = ACTION_MACRO_TAP(LSpec)
-    ,[Symbl] = ACTION_MACRO_TAP(Symbl)
-    ,[RMdia] = ACTION_MACRO_TAP(RMdia)
-    ,[RSpec] = ACTION_MACRO_TAP(RSpec)
+     [NUMPAD] = ACTION_MACRO_TAP(NUMPAD)
+    ,[SPECTG] = ACTION_MACRO_TAP(SPECTG)
+    ,[SYMBOL] = ACTION_MACRO_TAP(SYMBOL)
+    ,[MEDIA] = ACTION_MACRO_TAP(MEDIA)
 };
 
 uint16_t symb_shift = 0;
@@ -217,43 +212,19 @@ static uint16_t key_timer;
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     switch(id) {
-        // There are two shift keys for each layer so we increment a layer_shift var when one
-        // is pressed and decrement when one is released. If both are pressed at the same time
-        // then the layer is locked (or unlocked). The shift counts are bound between 0 and 2
-        // only because sometimes rapid pressing led to irregular events; this way the states
-        // are self healing during use.
 
-        case NmPad:                                               //
-        if (record->event.pressed) {                              // when the NmPad button is pressed
-            if(++symb_shift > 2) mdia_shift = 2;                  // increment the symb shift count, max two
-            if(spec_shift) symb_lock = !symb_lock;                // if the Special layer is on, toggle the shift lock
-            layer_on(SYMB);                                       // in any case, turn on the Symbols layer
-        } else {                                                  // when the NmPad button is released
-            if(--symb_shift < 0) symb_shift = 0;                  // decrement the shift count, minimum zero
-            if((!symb_shift) && (!symb_lock)) layer_off(SYMB);    // if both shifts are released and the lock is off, turn off the Symbols layer
-        }
-        break;
-
-        case LMdia:
+        case NUMPAD:
         if (record->event.pressed) {
-           if (record->tap.count && (!mdia_shift) && (!mdia_lock) && (!spec_shift)) {
-                register_code(KC_TAB);
-            } else {
-                if(spec_shift) mdia_lock = !mdia_lock;
-                if(++mdia_shift > 2) mdia_shift = 2;
-                layer_on(MDIA);
-            }
+            if(++symb_shift > 2) mdia_shift = 2;
+            if(spec_shift) symb_lock = !symb_lock;
+            layer_on(SYMB);
         } else {
-            if(record->tap.count && (!mdia_shift) && (!mdia_lock) && (!spec_shift)) {
-                unregister_code(KC_TAB);
-            } else {
-                if(--mdia_shift < 0) mdia_shift = 0;
-                if((!mdia_shift) && (!mdia_lock)) layer_off(MDIA);
-            }
+            if(--symb_shift < 0) symb_shift = 0;
+            if((!symb_shift) && (!symb_lock)) layer_off(SYMB);
         }
         break;
 
-        case LSpec:
+        case SPECTG:
         if (record->event.pressed) {
             if(symb_shift) symb_lock == !symb_lock;
             else if(mdia_shift) mdia_lock = !mdia_lock;
@@ -273,7 +244,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         }
         break;
 
-        case Symbl:
+        case SYMBOL:
         if (record->event.pressed) {
             if (record->tap.count && (!symb_shift) && (!symb_lock) && (!spec_shift)) {
                 register_code(KC_QUOT);
@@ -292,7 +263,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         }
         break;
 
-        case RMdia:
+        case MEDIA:
         if (record->event.pressed) {
             if (record->tap.count && (!mdia_shift) && (!mdia_lock) && (!spec_shift)) {
                 register_code(KC_BSLS);
@@ -311,25 +282,35 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         }
         break;
 
-        case RSpec:
+        case CO_PA: {
         if (record->event.pressed) {
-            if(symb_shift) symb_lock = !symb_lock;
-            else if(mdia_shift) mdia_lock = !mdia_lock;
-            else if (record->tap.count && !record->tap.interrupted && (!spec_shift)) {
-                register_code(KC_EQL);
-            } else {
-                if(++spec_shift > 2) spec_shift = 2;
-                layer_on(SPEC);
-            }
-        } else {
-            if(record->tap.count && !record->tap.interrupted && (!spec_shift)) {
-                unregister_code(KC_EQL);
-            } else {
-                if(--spec_shift < 0) spec_shift = 0;
-                if(!spec_shift) layer_off(SPEC);
-            }
-        }
-        break;
+                key_timer = timer_read();
+                }
+                else {
+                      if (timer_elapsed(key_timer) > 150) { // 150ms Treshhold
+                        return MACRO( D(LCTL), T(C), U(LCTL), END  );
+                        }
+                        else {
+                        return MACRO( D(LCTL), T(V), U(LCTL), END  );
+                        }
+                    }
+                    break;
+                }
+
+          case CU_PA: {
+          if (record->event.pressed) {
+                  key_timer = timer_read();
+                  }
+                  else {
+                      if (timer_elapsed(key_timer) > 150) { // 150ms Treshhold
+                        return MACRO( D(LCTL), T(X), U(LCTL), END  );
+                        }
+                        else {
+                        return MACRO( D(LCTL), T(V), U(LCTL), END  );
+                        }
+                    }
+                    break;
+                }
 
         // mouse diagonals
 
@@ -381,38 +362,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         }
         break;
 
-        case 30: { // start copy/paste macro
-            if (record->event.pressed) {
-                key_timer = timer_read(); // if the key is being pressed, we start the timer.
-            }
-            else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down").
-                if (timer_elapsed(key_timer) > 150) { // 150 being 150ms, the threshhold we pick for counting something as a tap.
-                    return MACRO( D(LCTL), T(C), U(LCTL), END  );
-                }
-                else {
-                    return MACRO( D(LCTL), T(V), U(LCTL), END  );
-                }
-            }
-            break;
-        } // end macro
-
-        case 31: { // start cut/paste macro
-            if (record->event.pressed) {
-                key_timer = timer_read(); // if the key is being pressed, we start the timer.
-            }
-            else { // this means the key was just released, so we can figure out how long it was pressed for (tap or "held down").
-                if (timer_elapsed(key_timer) > 150) { // 150 being 150ms, the threshhold we pick for counting something as a tap.
-                    return MACRO( D(LCTL), T(X), U(LCTL), END  );
-                }
-                else {
-                    return MACRO( D(LCTL), T(V), U(LCTL), END  );
-                }
-            }
-            break;
-        } // end macro
-
         default:
-            // none
             break;
     }
 
